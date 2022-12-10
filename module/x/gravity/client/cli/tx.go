f9ef448dbf2367438e99b9f30c7453ea3c26d362
Christian Borst <christianoborst@gmail.com>
2022-12-09 21:34:09 -0500

    Rename gravity tx build-batch to request-batch

--- module/x/gravity/client/cli/tx.go
+++ module/x/gravity/client/cli/tx.go
@@ -17,6 +17,7 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	paramstypesproposal "github.com/cosmos/cosmos-sdk/x/params/types/proposal"
 
 	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/gravity/keeper"
 	"github.com/Gravity-Bridge/Gravity-Bridge/module/x/gravity/types"
@@ -300,6 +301,7 @@ func CmdGovUnhaltBridgeProposal() *cobra.Command {
 	return cmd
 }
 
+// CmdSendToEth sends tokens to Ethereum. Locks Cosmos-side tokens into the Transaction pool for batching.
 func CmdSendToEth() *cobra.Command {
 	// nolint: exhaustruct
 	cmd := &cobra.Command{
@@ -385,11 +387,13 @@ func CmdCancelSendToEth() *cobra.Command {
 	return cmd
 }
 
+// CmdRequestBatch requests that the validators create and confirm a batch to be sent to Ethereum. This
+// is a manual command which duplicates the efforts of the Ethereum Relayer, likely not to be used often
 func CmdRequestBatch() *cobra.Command {
 	// nolint: exhaustruct
 	cmd := &cobra.Command{
-		Use:   "build-batch [token_contract_address]",
-		Short: "Build a new batch on the cosmos side for pooled withdrawal transactions",
+		Use:   "request-batch [token_contract_address]",
+		Short: "Request a new batch on the cosmos side for pooled withdrawal transactions",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx, err := client.GetClientTxContext(cmd)
